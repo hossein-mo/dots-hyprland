42302225dc183e559d55d51b4521d9aa32853c0a
Hossein Motahari <hossein.mo.c@gmail.com>
2025-09-06 15:18:14 +0300

    .

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -24,7 +24,11 @@ if status is-interactive
     alias lsd='lsd -l'
     alias pamcan=pacman
     alias ls='eza --icons'
+    alias pamcan pacman
+    alias ls 'eza --icons'
     alias clear "printf '\033[2J\033[3J\033[1;1H'"
+    alias q 'qs -c ii'
+
     set fish_greeting
 
 end
@@ -35,10 +39,5 @@ if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
     cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
 end
 
-    # Aliases
-    alias pamcan pacman
-    alias ls 'eza --icons'
-    alias clear "printf '\033[2J\033[3J\033[1;1H'"
-    alias q 'qs -c ii'
-    
-end
+# Aliases
+
